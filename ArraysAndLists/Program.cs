b1fd3ddf4362a154321ddec3f99a2ded8f6b3420
nate <nrice@truecoders.io>
2019-12-02 15:07:45 -0600

    init commit

---
+++ ArraysAndLists/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ArraysAndLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO:
+
+            // Create an int Array and populate numbers 1-10
+
+            /* Create two Lists of type int.
+             * Name one List "evens"
+             * Name the other List "odds"
+             */
+
+            /* Using either a foreach or for loop,
+             * nest an if statement to check to see
+             *  if a number is even or odd.
+             * Then add those numbers to either the evens List
+             * or the odds List
+             */
+
+            /* Now using foeach or for loops,
+             * display each List of even and odd numbers
+             *
+             * Try to be creative in your display
+             */
+        }
+    }
+}
